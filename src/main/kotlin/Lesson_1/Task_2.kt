31c91b7f27c8c89a68582b6ff884b6e178de5753
Hudzone <fyodor.blyudov@gmail.com>
2024-02-15 15:46:36 +0300

    Fixed variable time from var to val
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

--- src/main/kotlin/Lesson_1/Task_2.kt
+++ src/main/kotlin/Lesson_1/Task_2.kt
@@ -2,7 +2,7 @@ package Lesson_1
 
 fun main() {
     val thxLetter: String = "Спасибо за покупку!"
-    var ordersAmount: Int = 75
+    val ordersAmount: Int = 75
     var shopStaff: Int = 2000
 
     println("Количество заказов: $ordersAmount. $thxLetter")
