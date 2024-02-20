f195d1f13624aaa67cc9a16e6949773452a5fad0
Hudzone <fyodor.blyudov@gmail.com>
2024-02-20 22:04:58 +0300

    Lesson 2 Task 4: finished
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

---
+++ src/main/kotlin/Lesson_2/Task_4.kt
@@ -0,0 +1,17 @@
+package Lesson_2
+
+fun main() {
+    val crystalOre: Int = 7
+    val ironOre: Int = 11
+
+    println("Бонусная кристаллическая руда: ${miningRushBonus(crystalOre)}")
+    println("Бонусная железная руда: ${miningRushBonus(ironOre)}")
+
+}
+
+fun miningRushBonus(item: Int): Int {
+    val persentCalculation: Int = 100
+    val miningBuff: Int = 20
+    val buffValue: Int = (item * miningBuff) / persentCalculation
+    return buffValue
+}
