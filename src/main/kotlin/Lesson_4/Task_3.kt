cf120e3574ca1a9a77de70e3e32469b44c350d5f
Hudzone <fyodor.blyudov@gmail.com>
2024-03-04 11:21:46 +0300

    Lesson 4 Task 3: refactored
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

--- src/main/kotlin/Lesson_4/Task_3.kt
+++ src/main/kotlin/Lesson_4/Task_3.kt
@@ -2,13 +2,14 @@ package Lesson_4
 
 const val HUMIDIDTY_PASS: Int = 20
 const val TOY_NON_PASS: String = "зима"
+const val IS_SUNNY: Boolean = true
+const val IS_PROTECTED: Boolean = true
+
 fun main() {
-    val isSunny: Boolean = true
-    val isProtected: Boolean = true
     val humidity: Int = 20
     val timeOfYear: String = "зима"
 
-    val growthCondition: Boolean = isSunny && isProtected && humidity == HUMIDIDTY_PASS && timeOfYear != TOY_NON_PASS
+    val growthCondition: Boolean = IS_SUNNY && IS_PROTECTED && humidity == HUMIDIDTY_PASS && timeOfYear != TOY_NON_PASS
 
     println("Благоприятные ли условия сейчас для роста бобовых: $growthCondition")
 }
