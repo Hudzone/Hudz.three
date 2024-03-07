fb8d5ce1a9d3fde99935757865e8d411864ea687
Hudzone <fyodor.blyudov@gmail.com>
2024-03-07 18:19:17 +0300

    Lesson 4 Task 4: refactored
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

--- src/main/kotlin/Lesson_4/Task_4.kt
+++ src/main/kotlin/Lesson_4/Task_4.kt
@@ -1,14 +1,14 @@
 package Lesson_4
 
-const val wholeNum: Int = 2
+const val WHOLE_NUM: Int = 2
 fun main() {
     val trainingDay: Int = 5
 
     println("""
-        Упражнения для рук: ${trainingDay % wholeNum != 0}
-        Упражнения для ног: ${trainingDay % wholeNum == 0}
-        Упражнения для спины: ${trainingDay % wholeNum == 0}
-        Упражнения для пресса: ${trainingDay % wholeNum != 0}
+        Упражнения для рук: ${trainingDay % WHOLE_NUM != 0}
+        Упражнения для ног: ${trainingDay % WHOLE_NUM == 0}
+        Упражнения для спины: ${trainingDay % WHOLE_NUM == 0}
+        Упражнения для пресса: ${trainingDay % WHOLE_NUM != 0}
     """.trimIndent())
 
 }
