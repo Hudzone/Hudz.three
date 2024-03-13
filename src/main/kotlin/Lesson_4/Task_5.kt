12d76947c03ee70af27a3c69b2250ed71f037b99
Hudzone <fyodor.blyudov@gmail.com>
2024-03-13 12:07:38 +0300

    Lesson 4 Task 5: refactored
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

--- src/main/kotlin/Lesson_4/Task_5.kt
+++ src/main/kotlin/Lesson_4/Task_5.kt
@@ -4,6 +4,10 @@ const val MIN_SHIP_CREW = 55
 const val MAX_SHIP_CREW = 70
 const val MIN_FOOD_SUPPLY = 50
 const val REQUIRED_SHIP_CREW = 70
+const val GOOD_WEATHER = true
+const val BAD_WEATHER = false
+const val SHIP_DAMAGED = true
+const val SHIP_NON_DAMAGED = false
 
 fun main() {
     val isDamaged: Boolean = true
@@ -12,10 +16,9 @@ fun main() {
     val isWeatherGood: Boolean = true
 
     val isDepartureAvailable: Boolean =
-        (isDamaged == false && shipCrew in MIN_SHIP_CREW..MAX_SHIP_CREW
-                && foodSupply > MIN_FOOD_SUPPLY && isWeatherGood == true || false) ||
-                (isDamaged == true && shipCrew == REQUIRED_SHIP_CREW
-                        && foodSupply >= MIN_FOOD_SUPPLY && isWeatherGood == true)
+        (isDamaged == SHIP_NON_DAMAGED && shipCrew in MIN_SHIP_CREW..MAX_SHIP_CREW
+                && foodSupply > MIN_FOOD_SUPPLY) ||
+                (shipCrew == REQUIRED_SHIP_CREW && foodSupply >= MIN_FOOD_SUPPLY && isWeatherGood == GOOD_WEATHER)
 
     println("Благоприятны ли условия для отправки корабля: $isDepartureAvailable")
 }
