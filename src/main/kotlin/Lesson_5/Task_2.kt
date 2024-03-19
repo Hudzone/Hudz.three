40ef4340592f82503991466ee37a545f339e68c5
Hudzone <fyodor.blyudov@gmail.com>
2024-03-19 15:22:37 +0300

    Lesson 5 Task 2: readLine()!! -> readln()
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

--- src/main/kotlin/Lesson_5/Task_2.kt
+++ src/main/kotlin/Lesson_5/Task_2.kt
@@ -1,12 +1,13 @@
 package Lesson_5
 
 const val AGE_OF_MAJORITY = 18
-const val CURRENT_YEAR = 2024
 
 fun main() {
+    val currentYear = 2024
+
     print("Введите ваш год рождения: ")
     val yearOfBirth: Int = readln().toInt()
-    val calculateAge: Int = CURRENT_YEAR - yearOfBirth
+    val calculateAge: Int = currentYear - yearOfBirth
 
     if (calculateAge >= AGE_OF_MAJORITY) {
         println("Показать экран со скрытым контентом")
