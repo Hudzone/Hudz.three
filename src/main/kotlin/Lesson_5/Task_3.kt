0e2476023e0b69f6b166bf6748ecb7450301a6ac
Hudzone <fyodor.blyudov@gmail.com>
2024-03-19 18:25:06 +0300

    Lesson 5 Task 3: sent to code review
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

---
+++ src/main/kotlin/Lesson_5/Task_3.kt
@@ -0,0 +1,23 @@
+package Lesson_5
+
+fun main() {
+    val firstNum: Int = 13
+    val secondNum: Int = 40
+
+    println("Угадайте два числа от 0 до 42 и получите приз!")
+    print("Первое число: ")
+    val firstUserNum: Int = readLine()!!.toInt()
+    print("Второе число: ")
+    val secondUserNum: Int = readLine()!!.toInt()
+
+    if ((firstUserNum == firstNum || firstUserNum == secondNum)
+        && (secondUserNum == firstNum || secondUserNum == secondNum)) {
+        println("Поздравляю! Вы выиграли главный приз!")
+    } else if ((firstUserNum == firstNum || firstUserNum == secondNum)
+        || (secondUserNum == firstNum || secondUserNum == secondNum)) {
+        println("Вы выиграли утешительный приз!")
+    } else {
+        println("Неудача!")
+    }
+
+}
