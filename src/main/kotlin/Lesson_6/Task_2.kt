966724faa1fc97d12354a2b548ce6dbd09927763
Hudzone <fyodor.blyudov@gmail.com>
2024-03-25 13:36:17 +0300

    Lesson 6 Task 2: changed logic

--- src/main/kotlin/Lesson_6/Task_2.kt
+++ src/main/kotlin/Lesson_6/Task_2.kt
@@ -3,11 +3,11 @@ package Lesson_6
 fun main() {
     println("===ТАЙМЕР===")
     print("Сколько секунд нужно засечь: ")
-    var userInput: Int = readln().toInt()
+    val seconds = readLine()?.toInt() ?: return
 
-    while (userInput > 0) {
-        print("Прошло ${userInput--} сек.\r")
-        Thread.sleep(1000)
-    }
+    println("Таймер запущен на $seconds секунд")
 
+    Thread.sleep(seconds.toLong() * 1000)
+
+    println("Прошло $seconds секунд")
 }
