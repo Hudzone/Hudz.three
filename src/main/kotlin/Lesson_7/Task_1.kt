c48ecce1dea6e5761e549375f992b761b07013c2
Hudzone <fyodor.blyudov@gmail.com>
2024-03-30 18:16:15 +0300

    Lesson 7 Task 1: finished

--- src/main/kotlin/Lesson_7/Task_1.kt
+++ src/main/kotlin/Lesson_7/Task_1.kt
@@ -5,7 +5,7 @@ const val ITERATIONS = 3
 fun main() {
     println("===ГЕНЕРАТОР ПАРОЛЕЙ===")
     val literalRange: CharRange = 'a'..'z'
-    val numRange = 1..9
+    val numRange = 0..9
     var password = StringBuilder()
 
     for (i in 1..ITERATIONS) {
