edb8e4bbc458c9190504b4bf5450ac5b0fd755c8
Hudzone <fyodor.blyudov@gmail.com>
2024-04-12 16:08:50 +0300

    Lesson 8 Task 2: fixed
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

--- src/main/kotlin/Lesson_8/Task_2.kt
+++ src/main/kotlin/Lesson_8/Task_2.kt
@@ -6,14 +6,17 @@ fun main() {
     print("Проверка ингридиента. Введите название: ")
     val item: String = readln().toLowerCase()
 
+    var isFound = false
+
     for (i in recipeList) {
         if (i == item) {
             println("Ингредиент \"${i}\" в рецепте есть")
+            isFound = true
             break
-        } else {
-            continue
         }
     }
 
-    println("Такого ингредиента в рецепте нет")
+    if (isFound == false) {
+        println("Такого ингридиента в рецепте нет!")
+    }
 }
