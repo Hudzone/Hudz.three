7898fe3e4f62ce7af0c54088e8895d8da7399d4e
Hudzone <fyodor.blyudov@gmail.com>
2024-04-15 21:14:17 +0300

    Lesson 9 Task 4: finished

---
+++ src/main/kotlin/Lesson_9/Task_4.kt
@@ -0,0 +1,13 @@
+package Lesson_9
+
+fun main() {
+    println("Введите пять ингридиентов одной строкой разделяя их \",\":")
+    val userInput = readln()
+
+    val listOfIngridients = userInput.split(", ", ",")
+
+    val sortedList = listOfIngridients.sorted()
+
+    for (i in sortedList) println(i)
+}
+
