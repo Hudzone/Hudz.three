1d86ba188c9e49f7a5ee696c8a92db7e1124b215
Hudzone <fyodor.blyudov@gmail.com>
2024-04-17 22:37:58 +0300

    Lesson 10 Task 5: finished

---
+++ src/main/kotlin/Lesson_10/Task_5.kt
@@ -0,0 +1,53 @@
+package Lesson_10
+
+const val JWT_TOKEN_LENGTH = 32
+
+fun main() {
+    val userLogin = "faust"
+    val userPassword = "password"
+
+    print("Логин: ")
+    val typedLogin = readln()
+    print("Пароль: ")
+    val typedPassword = readln()
+
+    val authToken = createJWT(userLogin, userPassword, typedLogin, typedPassword)
+
+    getCart(authToken)
+
+}
+
+fun createJWT(username: String, password: String, userInput1: String, userInput2: String
+): String {
+
+    val engSym: CharRange = 'a'..'z'
+    val upEngSym: CharRange = 'A'..'Z'
+    val numSym: IntRange = 0..9
+    val symbolsPool = engSym.toList() + numSym.toList() + upEngSym.toList()
+    val jwtToken = StringBuilder()
+
+    if (username == userInput1 && password == userInput2) {
+
+        for (i in 1..JWT_TOKEN_LENGTH) {
+            val generatedSymbol = symbolsPool.random()
+            jwtToken.append(generatedSymbol)
+        }
+    } else {
+        jwtToken.append("null")
+    }
+
+    return jwtToken.toString()
+}
+
+fun getCart(jwtToken: String) {
+    val userCart = listOf("Картофель", "Курица", "Лук", "Чеснок", "Огурцы", "Ракета falcon-x")
+
+    if (jwtToken != "null") {
+        println("Ваш список продуктов: ")
+        var i = 0
+        userCart.forEach { product ->
+            println("${++i}. ${product}") }
+        } else {
+            println("Авторизация была неудачной!")
+    }
+}
