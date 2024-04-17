b607653128ae57eb267270e02923343fbf1867c5
Hudzone <fyodor.blyudov@gmail.com>
2024-04-17 18:52:01 +0300

    Lesson 10 Task 2: refactored
    Signed-off-by: Hudzone <fyodor.blyudov@gmail.com>

--- src/main/kotlin/Lesson_10/Task_2.kt
+++ src/main/kotlin/Lesson_10/Task_2.kt
@@ -4,29 +4,24 @@ const val MIN_CRED_LENGTH = 4
 
 fun main() {
 
-    userAuth()
+    print("Введите логин: ")
+    val userName = readln()
+    print("Введите пароль: ")
+    val userPassword = readln()
+
+    credentialValidation(userName, userPassword)
 
 }
-
-fun userLogin() : String = readln()
-fun userPass() : String = readln()
 
 fun credentialValidation(login: String, pass: String) {
     if (login.length < MIN_CRED_LENGTH || pass.length < MIN_CRED_LENGTH) {
         println("Логин или пароль недостаточно длинные!")
     } else {
-        println("""
+        println(
+            """
             Ваш логин: ${login}
             Ваш пароль: ${pass}
-        """.trimIndent())
+        """.trimIndent()
+        )
     }
-}
-
-fun userAuth() {
-    print("Введите логин: ")
-    val userName = userLogin()
-    print("Введите пароль: ")
-    val userPassword = userPass()
-
-    credentialValidation(userName, userPassword)
 }
