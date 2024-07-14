63020a3d37773674f9b8eed825ad09687c207dbc
Hudzone <fyodor.blyudov@gmail.com>
2024-07-14 09:51:10 +0300

    Lesson 11 Task 4: finished

---
+++ src/main/kotlin/lesson_11/Task_4.kt
@@ -0,0 +1,43 @@
+package lesson_11
+
+
+class Recipe(
+    var title: String,
+    var recipeCover: String,
+    var servings: Int,
+    var ingredients: Map<String, Int>,
+    var chosenRecipe: String,
+) {
+    fun servingsBar(value: Int) {
+        println("Отображаем логику выбора количества порций")
+
+    }
+
+    fun displayIngredients(servings: Int) {
+        println("Отображаем ингридиенты и грамовки, в зависимости от количества порций")
+    }
+
+    fun displayRecipe() {
+        // отображаем сам рецепт
+        println(chosenRecipe)
+    }
+
+}
+
+class RecipeCategoryScreen(
+    val category: List<String>,
+    val categoryName: String,
+    val pageCover: String,
+) {
+    fun displayCategories() {
+        println("Отображаем список с категориями")
+    }
+}
+
+class Ingredient(
+    val title: String,
+    val servingValue: Int,
+    val measureUnit: String,
+) {
+
+}
