007b2c47d4b084ce5b5fe8d1b4b5d9de7a8e8bf4
Hudzone <fyodor.blyudov@gmail.com>
2024-07-16 18:58:50 +0300

    Lesson 12 Task 1: fixed

--- src/main/kotlin/lesson_12/Task_1.kt
+++ src/main/kotlin/lesson_12/Task_1.kt
@@ -2,24 +2,36 @@ package lesson_12
 
 fun main() {
 
-    val july16 = WeatherMetrics(26, 20, false)
-    val july17 = WeatherMetrics(35, 26, true)
-    
+    val july16 = WeatherMetrics()
+    july16.dayTemp = 26
+    july16.nightTemp = 20
+    july16.isRainyToday = false
+
+    println("""
+            МЕТРИКИ:
+            Дневная температура: ${july16.dayTemp}
+            Ночная температура: ${july16.nightTemp}
+            Есть ли осадки: ${july16.isRainyToday}
+        """.trimIndent())
+
+    val july17 = WeatherMetrics()
+    july17.dayTemp = 35
+    july17.nightTemp = 29
+    july17.isRainyToday = true
+
+    println("""
+            МЕТРИКИ:
+            Дневная температура: ${july17.dayTemp}
+            Ночная температура: ${july17.nightTemp}
+            Есть ли осадки: ${july17.isRainyToday}
+        """.trimIndent())
+
 }
 
-class WeatherMetrics(_dayTemp: Int, _nightTemp: Int, _isRainyToday: Boolean) {
+class WeatherMetrics() {
 
-    var dayTemp: Int = _dayTemp
-    var nightTemp: Int = _nightTemp
-    var isRainyToday: Boolean = _isRainyToday
-
-    init {
-        println("""
-            МЕТРИКИ:
-            Дневная температура: ${dayTemp}
-            Ночная температура: ${nightTemp}
-            Есть ли осадки: ${isRainyToday}
-        """.trimIndent())
-    }
+    var dayTemp: Int = 10
+    var nightTemp: Int = 0
+    var isRainyToday: Boolean = true
 
 }
