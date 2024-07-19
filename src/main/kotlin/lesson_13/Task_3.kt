78ba5910b7603a28ebc3f9021eb222ac70f63ac6
Hudzone <fyodor.blyudov@gmail.com>
2024-07-19 14:11:51 +0300

    Lesson 13 Task 3: finished

---
+++ src/main/kotlin/lesson_13/Task_3.kt
@@ -0,0 +1,24 @@
+package lesson_13
+
+
+fun main() {
+
+    val contactList: List<ContactI> = listOf(
+        ContactI("User1", 84759837473, "Atlassian"),
+        ContactI("User2", 84759837473, null),
+        ContactI("User3", 84759837473, "null"),
+        ContactI("User4", 84759837473, null),
+        ContactI("User5", 84759837473, "Mundfish"),
+    )
+
+    contactList.forEach { contact -> contact.print() }
+
+}
+
+class ContactI(val name: String, val number: Long, val _company: String?) {
+    val company: String = _company ?: "<не указано>"
+
+    fun print() {
+        println("Имя: $name\nНомер: $number\nКомпания: $company\n")
+    }
+}
