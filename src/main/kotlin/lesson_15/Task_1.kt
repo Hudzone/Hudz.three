eb439c8e892dd7b0017876fff7b7ff1ad38b636b
Hudzone <fyodor.blyudov@gmail.com>
2024-07-26 18:37:21 +0300

    Lesson 15 Task 1: finished

---
+++ src/main/kotlin/lesson_15/Task_1.kt
@@ -0,0 +1,115 @@
+package lesson_15
+
+//В мобильной игре есть разные существа. Напиши интерфейсы для летающих и плавающих существ.
+//
+//- создай интерфейсы с методами передвижения для: карась, чайка, утка;
+//- создай для каждого существа классы, реализующие соответствующие интерфейсы;
+//- выведи сообщения в консоль, о том как они могут передвигаться.
+
+fun main() {
+
+    val fish = Crucian("Карась")
+    val seagull = Seagull("Чайка")
+    val duck = Duck("Утка")
+
+    fish.swim()
+    fish.diveIn()
+    fish.popUp()
+
+    seagull.diveIn()
+    seagull.fly()
+    seagull.land()
+    seagull.takeOff()
+
+    duck.swim()
+    duck.diveIn()
+    duck.fly()
+    duck.land()
+    duck.takeOff()
+    
+}
+
+interface Waterfowl {
+
+    fun swim() {}
+
+    fun diveIn() {}
+
+    fun popUp() {}
+
+}
+
+interface Flying {
+
+    fun fly() {}
+
+    fun land() {}
+
+    fun takeOff() {}
+
+}
+
+abstract class Creature : Waterfowl, Flying {
+
+    abstract val name: String
+
+}
+
+class Crucian(override val name: String) : Creature(), Waterfowl {
+
+    override fun swim() {
+        println("$name плывет")
+    }
+
+    override fun diveIn() {
+        println("$name погружается")
+    }
+
+    override fun popUp() {
+        println("$name всплывает")
+    }
+
+}
+
+class Seagull(override val name: String) : Creature() {
+
+    override fun diveIn() {
+        println("$name ныряет")
+    }
+
+    override fun fly() {
+        println("$name летит")
+    }
+
+    override fun land() {
+        println("$name приземляется")
+    }
+
+    override fun takeOff() {
+        println("$name взлетает")
+    }
+}
+
+class Duck(override val name: String) : Creature() {
+
+    override fun swim() {
+        println("$name плывет по поверхности")
+    }
+
+    override fun diveIn() {
+        println("$name ныряет")
+    }
+
+    override fun fly() {
+        println("$name летит")
+    }
+
+    override fun land() {
+        println("$name приземляется")
+    }
+
+    override fun takeOff() {
+        println("$name взлетает")
+    }
+
+}
