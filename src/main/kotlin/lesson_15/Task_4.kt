0afbfd3852db4dd4f69f0bbed83ea7a396b73eea
Hudzone <fyodor.blyudov@gmail.com>
2024-07-27 15:50:32 +0300

    Lesson 15 Task 4: finished

---
+++ src/main/kotlin/lesson_15/Task_4.kt
@@ -0,0 +1,54 @@
+package lesson_15
+
+fun main() {
+
+    val guitar = Instrument("Gibson LesPaul Standart", 12)
+    val strings_10 = InstrumentParts(guitar, "Струны 10 мм", 33)
+    val strings_12 = InstrumentParts(guitar, "Струны 12 мм", 22)
+    val tuner = InstrumentParts(guitar, "Тюнер цифровой Sony", 10)
+
+    guitar.search()
+
+
+}
+
+interface Searchable {
+
+    fun search()
+
+}
+
+abstract class Item {
+    abstract val name: String
+    abstract val quantity: Int
+}
+
+class Instrument(
+    override val name: String,
+    override val quantity: Int,
+    var parts: MutableList<InstrumentParts> = mutableListOf(),
+) : Item(), Searchable {
+    override fun search() {
+        val animationUnit = listOf(".", ".", ".", ".", ".\n")
+        print("Выполняется поиск")
+        for (i in animationUnit) {
+            print(i)
+            Thread.sleep(500)
+        }
+        println("Комплектующие: ")
+        parts.forEach { it ->
+            println("Товар: ${it.name} Количество на складе: ${it.quantity}")
+        }
+    }
+}
+
+class InstrumentParts(
+    val fatherLayer: Instrument,
+    override val name: String,
+    override val quantity: Int,
+) : Item() {
+    init {
+        fatherLayer.parts.add(this)
+    }
+}
+
