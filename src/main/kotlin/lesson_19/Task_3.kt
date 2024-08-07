55114a176e005ed0dd6b9b1ecc2a68f41868c846
Hudzone <fyodor.blyudov@gmail.com>
2024-08-07 20:25:41 +0300

    Lesson 19 Task 3: finished

---
+++ src/main/kotlin/lesson_19/Task_3.kt
@@ -0,0 +1,25 @@
+package lesson_19
+
+fun main() {
+
+    val ship = Starship()
+    ship.fly()
+    ship.land()
+    ship.railGun()
+
+}
+
+class Starship {
+
+    fun fly() {
+        // TODO("Нужна информация о дополнительной логике")
+    }
+
+    fun land() {
+        // TODO("Нужна информация о дополнительной логике")
+    }
+
+    fun railGun() {
+        TODO("Ship doesn't have a railgun")
+    }
+}
